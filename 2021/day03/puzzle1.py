43c3b1144d9b3ccda0134f1630d28dc9c81129e0
Adam Denyer <github@adamdenyer.com>
2021-12-03 14:06:45 +0000

    Day 3 puzzle 1 complete

--- 2021/day03/puzzle1.py
+++ 2021/day03/puzzle1.py
@@ -1,9 +1,29 @@
-binString = ''
-gamma = bin(0)
-epsilon = bin(0)
-
-with open('/home/adam/Documents/Repos/adventofcode/2021/day03/example.txt') as txtfile:
+with open('/home/adam/Documents/Repos/adventofcode/2021/day03/input.txt') as txtfile:
     input = list(map(str, txtfile.read().split('\n')))
 
-for i in range(len(input[0])):
+gammaString = '#' * len(input[0])
+epsilonString = '#' * len(input[0])
+
+for bit in range(len(input[0])):
+    ones = zeroes = count = 0
+
+    for line in input:
+        ones += int(line[bit])
+        count += 1
     
+    zeroes = count-ones
+
+    if ones > zeroes:
+        mostCommon = 1
+        leastCommon = 0
+    elif zeroes > ones:
+        mostCommon = 0
+        leastCommon = 1
+
+    gammaString = gammaString[:bit] + str(mostCommon) + gammaString[bit+1:]
+    epsilonString = epsilonString[:bit] + str(leastCommon) + epsilonString[bit+1:]
+
+gamma = int(gammaString,2)
+epsilon = int(epsilonString,2)
+
+print(gamma*epsilon)
